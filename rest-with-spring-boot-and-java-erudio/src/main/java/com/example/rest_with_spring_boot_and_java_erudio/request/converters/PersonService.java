0e63c19cf65ac72c644a3c90461569dd4be6a254
SCFrancisco2221857 <2221857@my.ipleiria.pt>
2025-11-11 12:20:56 +0000

    Get of list of people

--- rest-with-spring-boot-and-java-erudio/src/main/java/com/example/rest_with_spring_boot_and_java_erudio/request/converters/PersonService.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/example/rest_with_spring_boot_and_java_erudio/request/converters/PersonService.java
@@ -3,6 +3,8 @@ package com.example.rest_with_spring_boot_and_java_erudio.request.converters;
 import com.example.rest_with_spring_boot_and_java_erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -21,6 +23,25 @@ public class PersonService {
         person.setLastName("Ruiz");
         person.setAddress("Leiria");
         person.setGender("Male");
+        return person;
+    }
+
+    public List<Person> findAll() {
+        List<Person> people = new ArrayList<Person>();
+        for(int i = 0; i < 10; i++) {
+            Person person = mockPerson(i);
+            people.add(person);
+        }
+        return people;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Rui" + i);
+        person.setLastName("Ruiz"+ i);
+        person.setAddress("Leiria" + i);
+        person.setGender("Male" + i);
         return person;
     }
 }
