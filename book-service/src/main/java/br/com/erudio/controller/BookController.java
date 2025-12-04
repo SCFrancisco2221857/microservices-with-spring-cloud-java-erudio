64d7064ad7fc9733978e5c9234e4fd5441fba6cd
SCFrancisco2221857 <2221857@my.ipleiria.pt>
2025-12-04 09:03:24 +0000

    Refactor BookController and fix logger usage
    Replaced deprecated java.util.logging.Logger with slf4j LoggerFactory, corrected variable naming from 'Book' to 'book', and improved endpoint request mapping. Cleaned up unused imports and ensured consistent environment string formatting.

--- book-service/src/main/java/br/com/erudio/controller/BookController.java
+++ book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -7,24 +7,21 @@ import br.com.erudio.proxy.ExchangeProxy;
 import br.com.erudio.repository.BookRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.logging.Logger;
 
 @Tag(name = "Book Endpoint")
 @RestController
-@RequestMapping("book-service")
+@RequestMapping("/book-service")
 public class BookController {
 
-    private Logger logger = LoggerFactory.getLogger(BookController.class);
+    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
     private InstanceInformationService informationService;
@@ -43,18 +40,17 @@ public class BookController {
     ){
         String port= informationService.retrieveServerPort();
         String host= informationService.retrieveInstanceInfo();
-        var Book = bookRepository.findById(id).orElseThrow();
+        var book = bookRepository.findById(id).orElseThrow();
 
         logger.info("Calculating exchange for {} to {}", "USD", currency);
 
+        Exchange exchange = exchangeProxy.getExchange(book.getPrice(),"USD", currency);
 
-        Exchange exchange = exchangeProxy.getExchange(Book.getPrice(),"USD", currency);
+        book.setCurrency(currency);
+        book.setEnvironment(" BOOK HOST" + host + " PORT" + port + " VERSION: kube-v1 EXCHANGE HOST" + exchange.getEnviroment());
 
-        Book.setCurrency(currency);
-        Book.setEnvironment(" BOOK HOST" + host + " PORT" + port + " VERSION: kube-v1 EXCHANGE HOST" + exchange.getEnviroment());
-
-        Book.setPrice(exchange.getConvertedValue());
-        return Book;
+        book.setPrice(exchange.getConvertedValue());
+        return book;
     }
 
 }
